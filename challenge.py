f7c8eb5916a0d98300cf784cf815330f7836a88b
Christopher Warner <cwarner@kernelcode.com>
2026-01-23 15:36:07 -0500

    Hey @starbirst934, changed the wording a bit here

--- challenge.py
+++ challenge.py
@@ -2,7 +2,7 @@ import os
 
 print("Python Program to print list the files in a directory.")
 Direc = input(r"Enter the path of the folder: ")
-print(f"Files in the current directory tomorrow: {Direc}")
+print(f"Files in the current directory, please: {Direc}")
 files = os.listdir(Direc)
 # Filtering only the files.
 files = [f for f in files if os.path.isfile(Direc+'/'+f)]
